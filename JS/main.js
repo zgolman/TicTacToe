2455e8344328450a3a7547b3ccdbbae10aa2f596
Zachary Golman <zacharygolman@Zacharys-MacBook-Air.local>
2014-10-14 16:09:49 -0700

    boxes are clickable

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,37 @@
 var ticApp = angular.module('ticApp', []);
 
  ticApp.controller('TTTController',["$scope", function ($scope){
-	$scope.board = [
-				{status:"A", pos:0},{status:"A", pos: 1},{status:"A", pos: 2},
-				{status:"A", pos:3},{status:"A", pos: 4},{status:"A", pos: 5},
-				{status:"A", pos:6},{status:"A", pos: 7},{status:"A", pos: 8}];
 
-				$scope.makeMove = function(){
-					alert(this.$index);
-					alert(mySingleCell.status)
-				};
-			}]);
+
+	$scope.board = [
+				{status:"A", pos:0, played:"False"},{status:"A", pos: 1, played:"False"},{status:"A", pos: 2, played:"False"},
+				{status:"A", pos:3, played:"False"},{status:"A", pos: 4, played:"False"},{status:"A", pos: 5, played:"False"},
+				{status:"A", pos:6, played:"False"},{status:"A", pos: 7, played:"False"},{status:"A", pos: 8, played:"False"}];
+    
+    $scope.movecounter = 0 ;
+
+	$scope.playerPicks = function(oneCellObject) {
+    $scope.movecounter = $scope.movecounter + 1 ;
+    console.log("Cell was: " + oneCellObject.Status) ;
+    if (($scope.movecounter % 2) == 1) {
+      oneCellObject.Status = "X" ;  
+    } else {
+      oneCellObject.Status = "O" ;
+    } 
+    console.log("Cell is now: " + oneCellObject.Status) ;
+  	} ;
+	
+}]);
+
+
+
+
+
+ // $scope.makeMove = function(){
+	// 			alert(this.$index);
+	// 			alert(mySingleCell.status)
+	// 			};
+
+		// $scope.testJS = function() {
+  //   console.log('Correctly accessing JS function.') ;
+  //    } ;
